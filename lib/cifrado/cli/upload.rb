9c151728665ba553755258c8878b07f3c52705ba
Sergio Rubio <rubiojr@frameos.org>
2013-02-28 00:59:19 +0100

    Added support to upload multiple files to 'upload'

--- lib/cifrado/cli/upload.rb
+++ lib/cifrado/cli/upload.rb
@@ -1,59 +1,83 @@
 module Cifrado
   class CLI
-    desc "upload CONTAINER FILE", "Upload a file"
-    option :encrypt, :desc => 'Encrypt: a:recipient (asymmetric) or symmetric'
-    option :segments, :type => :numeric, :desc => "Split the data in segments"
-    option :strip_path, :type => :boolean
-    option :progressbar, :default => :fancy
-    option :bwlimit, :type => :numeric
-    option :force, :type => :boolean
-    def upload(container, file)
-      unless file and File.exist?(file)
-        raise "File '#{file}' does not exist"
-      end
+    desc "upload CONTAINER FILE1 [FILE2] ...",
+         "Upload files or directories"
+    option :encrypt,
+           :desc => 'Encrypt files when uploading'
 
-      client = client_instance 
+    option :segments, 
+           :type => :numeric, 
+           :desc => "Split the data into segments"
 
-      tstart = Time.now
-      uploaded = nil
-      files = []
-      if File.directory?(file)
-        files = Dir["#{file}/**/*"].reject { |f| File.directory?(f) }
-      else
-        files << file
+    option :strip_path, 
+           :type => :boolean,
+           :desc => 'Strip path from file when uploading'
+
+    option :progressbar,
+           :default => :fancy,
+           :desc => 'Progressbar style'
+
+    option :bwlimit,
+           :type => :numeric,
+           :desc => 'Limit the bandwidth available for uploads'
+
+    option :force, 
+           :type => :boolean,
+           :desc => 'Overwrite files when uploading'
+    def upload(container, *args)
+
+      if args.empty?
+        help 'upload'
+        raise "No files specified"
       end
 
       uploaded = []
-      files.each do |f|
-        begin
-          if options[:segments]
-            uploaded << split_and_upload(client, container, f)
-          else
-            headers = client.head container, clean_object_name(f)
-            if headers
-              if headers['Etag'] == Digest::MD5.file(f).to_s
-                if options[:force]
-                  Log.warn "File #{f} already uploaded and MD5 matches."
-                  Log.warn "Since --force was used, uploading it again."
-                  uploaded << upload_single(client, container, f)
+      args.each do |file|
+        unless file and File.exist?(file)
+          raise "File '#{file}' does not exist"
+        end
+
+        client = client_instance 
+
+        tstart = Time.now
+        files = []
+        if File.directory?(file)
+          files = Dir["#{file}/**/*"].reject { |f| File.directory?(f) }
+        else
+          files << file
+        end
+
+        files.each do |f|
+          begin
+            if options[:segments]
+              uploaded << split_and_upload(client, container, f)
+            else
+              headers = client.head container, clean_object_name(f)
+              if headers
+                if headers['Etag'] == Digest::MD5.file(f).to_s
+                  if options[:force]
+                    Log.warn "File #{f} already uploaded and MD5 matches."
+                    Log.warn "Since --force was used, uploading it again."
+                    uploaded << upload_single(client, container, f)
+                  else
+                    Log.warn "File #{f} already uploaded and MD5 matches, skipping."
+                  end
                 else
-                  Log.warn "File #{f} already uploaded and MD5 matches, skipping."
+                  Log.warn "File #{f} already uploaded, but it has changed."
+                  if options[:force]
+                    Log.warn "Overwriting it as requested (--force)."
+                    uploaded << upload_single(client, container, f)
+                  else
+                    Log.warn "Since --force was not used, skipping it."
+                  end
                 end
               else
-                Log.warn "File #{f} already uploaded, but it has changed."
-                if options[:force]
-                  Log.warn "Overwriting it as requested (--force)."
-                  uploaded << upload_single(client, container, f)
-                else
-                  Log.warn "Since --force was not used, skipping it."
-                end
+                uploaded << upload_single(client, container, f)
               end
-            else
-              uploaded << upload_single(client, container, f)
             end
+          rescue Errno::ENOENT => e
+            Log.error "Error uploading #{f}: " + e.message
           end
-        rescue Errno::ENOENT => e
-          Log.error "Error uploading #{f}: " + e.message
         end
       end
       uploaded.flatten
