1233fc7667def93994eeaada11d67345347942a0
Sergio Rubio <rubiojr@frameos.org>
2013-03-03 14:42:46 +0100

    cleanup old StreamingUploader comments, no longer apply after refactoring

--- lib/cifrado/streaming_uploader.rb
+++ lib/cifrado/streaming_uploader.rb
@@ -1,19 +1,5 @@
 module Cifrado 
 
-  # StreamingUploader Adapted by Sergio Rubio <rubiojr@frameos.org>
-  #
-  # inspired by Opscode Chef StreamingCookbookUploader chef/streaming_cookbook_uploader.rb
-  # http://opscode.com
-  # 
-  # inspired by/cargo-culted from http://stanislavvitvitskiy.blogspot.com/2008/12/multipart-post-in-ruby.html
-  # On Apr 6, 2010, at 3:00 PM, Stanislav Vitvitskiy wrote:
-  #
-  # It's free to use / modify / distribute. No need to mention anything. Just copy/paste and use.
-  #
-  # Regards,
-  # Stan
-
-
   require 'net/http'
   require 'net/https'
   require 'cifrado/rate_limit'
